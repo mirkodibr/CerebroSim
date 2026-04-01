7f827ed13e0b19031d87d972750e71be9ff54b60
mirkodibr <mdibr22@stlawu.edu>
2026-04-01 01:57:07 -0400

    Prompt 3: Create SimulationConstants (Requirement: Simulation Constants)

---
+++ lib/models/simulation_constants.dart
@@ -0,0 +1,10 @@
+class SimulationConstants {
+  static const double kDefaultLearningRate = 0.01;
+  static const double kDefaultDecayRate = 0.1;
+  static const double kDefaultThreshold = 1.0;
+  static const int kTickRateHz = 60;
+  static const double kDefaultHeadVelAmplitude = 40.0;
+  static const double kDefaultVorFrequencyHz = 1.0;
+  static const double kHealthyVorGain = 1.0;
+  static const double kAtaxiaVorGain = 0.4;
+}
